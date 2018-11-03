eb8a5ea49c8a076113b58c1f300e240f80244f38
IntenseWizard <33976765+IntenseWizard@users.noreply.github.com>
2018-11-03 14:37:42 +0200

    some dependencies

--- ipynb_to_py_and_run.sh
+++ ipynb_to_py_and_run.sh
@@ -5,6 +5,10 @@ jupyter nbconvert --to python carPrices_nettiauto.ipynb
 jupyter nbconvert --to python carPrices_autotalli.ipynb
 
 #run with python3
-python3 carPrices_autotie.py
-python3 carPrices_nettiauto.py
-python3 carPrices_autotalli.py
+# python3 carPrices_autotie.py
+# python3 carPrices_nettiauto.py
+# python3 carPrices_autotalli.py
+
+ #pip3 install requests
+ #sudo apt install python3-bs4
+ #sudo apt install python3-psutil
