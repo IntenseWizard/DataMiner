2b890d501d57b912ad84af9899fbff59f31c54d4
Your Name <you@example.com>
2019-03-30 18:02:38 +0200

    analysis

--- stock/analyze_with_r.R
+++ stock/analyze_with_r.R
@@ -1,37 +1,36 @@
 install.packages('devtools')
-library(devtools)
 install.packages("Hmisc")
-library(Hmisc)
 install.packages("psych")
-library(psych)
 install.packages('plotly')
-library(plotly)
 install.packages('forecast')
-library(forecast)
 install.packages('tseries')
+install.packages('zoo')#,dependencies=TRUE
+install.packages('smooth')
+install.packages('dplyr')
+install.packages('readr')
+install.packages('zoo')
+install.packages('tidyverse',dependencies=TRUE)
+
+library(devtools)
+library(Hmisc)
+library(psych)
+library(plotly)
+library(forecast)
 library(tseries)
-install.packages('zoo', dependencies = TRUE)
 library(zoo)
-# remove.packages("quantmod")
-# remove.packages("TTR")
-# remove.packages("tseries")
-
-#setwd('/home/m/DataMiner/stock')
-data_dir='./stock_data/'
-#Read data
-files=list.files(path = data_dir, pattern = NULL, all.files = FALSE,
-           full.names = FALSE, recursive = FALSE,
-           ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
-print(files)
-file=files[1]
-
-data <- read.csv(file=paste(data_dir,files,sep=""), header=TRUE, sep=";")
-
-#install.packages('dplyr')
-#install.packages('readr')
+library(smooth)
 library(dplyr)
 library(readr)
-files = list.files(path = data_dir, full.names = TRUE)
+library(tidyverse)
+library(zoo)
+setwd('/home/m/DataMiner/stock')
+data_dir='./stock_data/'
+#Read data
+files=list.files(path = data_dir, pattern = '*INTC.csv', all.files = FALSE,
+           full.names = TRUE, recursive = FALSE,
+           ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+print(files)
+
 df= lapply(files,read.csv,header=TRUE, sep=";")
 
 for (i in c(1:length(df))) {
@@ -42,16 +41,20 @@ for (i in c(1:length(df))) {
 }
 colnames(n)=c("Date","Open", "High", "Low", "Close", "Volume")
 
-data=n[order(n["Date"]),]
+raw_data=n[order(n["Date"],deccreasing="TRUE"),]
+data=raw_data[duplicated(raw_data)==FALSE,]
+ts_data=as.ts(data)
+ma_30=rollmean(data[["High"]],300)
+
+plot(data[["High"]])
+lines(ma_30)
+head(data)
 
 length(data["High",])
 length(data[["Open"]])
 
-plot(data[["Date"]],data[["High"]])
+plot.ts(data[["Date"]],data[["High"]])
 
-
-install.packages('smooth')
-library(smooth)
 data_high=as.ts(data[["High"]])
 
 data_high_sma=sma(data_high, h=3)
@@ -59,8 +62,7 @@ data_high_sma=sma(data_high, h=3)
 plot.ts(data_high)
 plot.ts(data_high_sma)
 
-movavg(x, n, type=c("s"))
-rollMonthlyWindows(data["Open"], period = "12m", by = "1m")
+
 
 # for(file in files){
   # print(file)
